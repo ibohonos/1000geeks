a15834ea60bb359446668cdc6c7463bd9ef98161
vagrant <vagrant@homestead>
2018-11-16 13:17:41 +0000

    convert spaces to tabs

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,13 +7,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    /**
-     * @Route("/", name="home")
-     */
-    public function index()
-    {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
-    }
+	/**
+	 * @Route("/", name="home")
+	 */
+	public function index()
+	{
+		return $this->render('home/index.html.twig', [
+			'controller_name' => 'HomeController',
+		]);
+	}
 }
--- src/Entity/Code.php
+++ src/Entity/Code.php
@@ -9,63 +9,63 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Code
 {
-    /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+	/**
+	 * @ORM\Id()
+	 * @ORM\GeneratedValue()
+	 * @ORM\Column(type="integer")
+	 */
+	private $id;
 
-    /**
-     * @ORM\Column(type="string", length=10, unique=true)
-     */
-    private $code;
+	/**
+	 * @ORM\Column(type="string", length=10, unique=true)
+	 */
+	private $code;
 
-    /**
-     * @ORM\Column(type="datetime")
-     */
-    private $date;
+	/**
+	 * @ORM\Column(type="datetime")
+	 */
+	private $date;
 
-    public function getId(): int
-    {
-        return $this->id;
-    }
+	public function getId(): int
+	{
+		return $this->id;
+	}
 
 	/**
 	 * @return string
 	 */
 	public function getCode(): string
-    {
-        return $this->code;
-    }
+	{
+		return $this->code;
+	}
 
 	/**
 	 * @param string $code
 	 * @return Code
 	 */
 	public function setCode(string $code): self
-    {
-        $this->code = $code;
+	{
+		$this->code = $code;
 
-        return $this;
-    }
+		return $this;
+	}
 
 	/**
 	 * @return \DateTimeInterface|null
 	 */
 	public function getDate(): ?\DateTimeInterface
-    {
-        return $this->date;
-    }
+	{
+		return $this->date;
+	}
 
 	/**
 	 * @param \DateTimeInterface $date
 	 * @return Code
 	 */
 	public function setDate(\DateTimeInterface $date): self
-    {
-        $this->date = $date;
+	{
+		$this->date = $date;
 
-        return $this;
-    }
+		return $this;
+	}
 }
